eda635f2cc337edfe1b908f60ade15c288ecb271
Will Bao <willb@uci.edu>
2026-01-31 21:18:29 -0800

    added url parsing

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin, urlunparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,29 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    if (resp.status != 200):
+        print(f"Failed to retrieve the webpage. Status code: {resp.status}")
+
+    found_links = list()
+
+    soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+
+    extracted_links = soup.find_all("a", href=True)
+
+    for tag in extracted_links:
+        # Convert to full url
+        href = tag.get("href")
+        full_url = urljoin(resp.url, href)
+
+        if is_valid(full_url):
+            # Defragment url
+            parsed = urlparse(url)
+            defragmented_url = urlunparse(parsed._replace(fragment=""))
+            
+            found_links.append(defragmented_url)
+
+    return found_links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
